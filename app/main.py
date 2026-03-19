6fcb2da78121d645fcd7c6e4fabd8fd8c15048bc
Oleh Halas <oleggalas8@gmail.com>
2026-03-19 11:48:43 +0200

    Implement Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result = {}
+
+    @wraps(func)
+    def inner(*args: Callable, **kwargs: Callable) -> Any:
+
+        try:
+            key = args
+            if kwargs:
+                key = args + tuple(sorted(kwargs.items()))
+        except TypeError:
+            raise TypeError("Arguments must be hashable")
+
+        if key in result:
+            print("Getting from cache")
+            return result[key]
+        else:
+            print("Calculating new result")
+            result[key] = func(*args, **kwargs)
+            return result[key]
+
+    return inner
